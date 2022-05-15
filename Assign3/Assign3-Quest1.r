d1b0580bd04092da9f209b5aff4fcc7b63a9719a
Renad <renad.gharz@outlook.com>
2022-05-15 14:58:36 -0600

    Question 1a-1.e

---
+++ Assign3/Assign3-Quest1.r
@@ -0,0 +1,41 @@
+#Loading libraries
+library(readxl)
+
+#Loading data
+mt_scores <- read_xlsx("./Assign3/Assign3Data.xlsx", sheet = "Midterm Scores")
+
+#1.a
+
+
+
+
+#1.b
+
+
+
+
+#1.c
+
+
+
+
+#1.d
+
+
+
+
+#1.e
+
+
+
+
+#1.f
+
+
+
+
+#1.g
+
+
+
+
--- Assign3/Assign3-Quest2.r
+++ Assign3/Assign3-Quest2.r
@@ -1,1 +1,29 @@
+#Loading libraries
+library(readxl)
+
+#Loading data
+util_bill <- read_xlsx("./Assign3/Assign3Data.xlsx", sheet = "Utility Bills")
+
+#2.a
+
+
+
+
+#2.b
+
+
+
+
+#2.c
+
+
+
+
+#2.d
+
+
+
+
+#2.e
+
 
